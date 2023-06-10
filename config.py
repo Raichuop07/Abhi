65302f3a5d303e620cd17ebac19ec7d286c7c11d
SATYAM <126665079+Raichuop07@users.noreply.github.com>
2023-06-10 22:53:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "ᴀɴᴏɴ ダ ᴍᴜsɪᴄ")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "❤️◄⏤͟͟͞🍃𝐅𝐑𝐈🅔︎𝐍🅓︎💛 🅩︎𝐎𝐍𝐄💚𝐌𝐔🅢︎𝐈𝐂🛡🔮")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1356469075").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6190749761").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
